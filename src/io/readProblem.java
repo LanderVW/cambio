a7a19898a368c04f14de552397c4c6d958a9bb82
axel vulsteke <axel.vulsteke@student.kuleuven.be>
2017-11-17 10:35:34 +0100

    d

--- src/io/readProblem.java
+++ src/io/readProblem.java
@@ -1,4 +1,16 @@
 package io;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class readProblem {
+    //read in problem and make requests, zones, and zone tot zone matrix
+
+    String csvFile = "/toy.csv/";
+    BufferedReader br = null;
+    String line = "";
+    String cvsSplitBy = ",";
+
 }
