b07344a3e9a2d0ec3b76bc903a6629904b566ae0
axel vulsteke <axel.vulsteke@student.kuleuven.be>
2017-11-17 10:50:00 +0100

    read in

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import io.readProblem;
 public class Main {
     public static void main(String [] args){
 
 
         int[][] zonesToZone = new int[10][10];
-
-
+        readProblem r = new readProblem();
+        r.readIn();
     }
 }
--- src/io/readProblem.java
+++ src/io/readProblem.java
@@ -8,9 +8,18 @@ import java.io.IOException;
 public class readProblem {
     //read in problem and make requests, zones, and zone tot zone matrix
 
-    String csvFile = "/toy.csv/";
+    String csvFile = "toy1.csv";
     BufferedReader br = null;
     String line = "";
-    String cvsSplitBy = ",";
+    String cvsSplitBy = ";";
+
+    public void readIn() {
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+    }
 
 }
