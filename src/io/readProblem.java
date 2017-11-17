0f0d0d41b352d66d8c7ab8d6cc3f11f3e4d1d90f
axel vulsteke <axel.vulsteke@student.kuleuven.be>
2017-11-17 11:19:40 +0100

    busy

--- src/io/readProblem.java
+++ src/io/readProblem.java
@@ -12,13 +12,33 @@ public class readProblem {
     BufferedReader br = null;
     String line = "";
     String cvsSplitBy = ";";
+    Integer numberOfRequests;
+    String[] tmp;
 
     public void readIn() {
         try {
             br = new BufferedReader(new FileReader(csvFile));
+            line = br.readLine();
+            tmp = line.split(":");
+            numberOfRequests = Integer.parseInt(tmp[1]);
+            int request_id, day_index, start_time, duration, penalty1, penalty2;
+            String zone_id;
+            String [] possible_vehicle_list;
+            for (int i = 0; i < numberOfRequests ; i++) {
+                tmp = line.split(cvsSplitBy);
+                request_id= Integer.parseInt(tmp[0]);
+                zone_id = tmp[1];
+            }
 
+
+            while ((line = br.readLine()) != null) {
+                String[] test = line.split(":");
+                System.out.println(test[1]);
+            }
         } catch (FileNotFoundException e) {
             System.out.println(e);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
